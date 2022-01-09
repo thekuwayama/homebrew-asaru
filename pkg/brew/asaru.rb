233c203a43fcc7a3b89b2c09dac36cc68f473e31
thekuwayama <thekuwayama@gmail.com>
2022-01-10 02:36:11 +0900

    aarch64-apple-darwin

--- pkg/brew/asaru.rb
+++ pkg/brew/asaru.rb
@@ -11,10 +11,13 @@ class Asaru < Formula
   desc 'Asana Tasks Search CLI'
   homepage 'https://github.com/thekuwayama/asaru'
 
-  if OS.mac?
+  if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/thekuwayama/asaru/releases/download/#{version}/asaru-#{version}-x86_64-apple-darwin.tar.gz"
     sha256 '2752996c222d992d97a90884a48b7e205035d982e90f5e6eaacd15b96159f428'
-  elsif OS.linux?
+  elsif OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/thekuwayama/asaru/releases/download/#{version}/asaru-#{version}-aarch64-apple-darwin.tar.gz"
+    sha256 '3c928139fd905c0522063717cd7f8674a1433f8d5f3d829b864d1137ae66729b'
+  else
     warn '** not support **'
     exit 1
   end
