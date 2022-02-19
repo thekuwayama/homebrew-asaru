5ca2c4519d4f2caa9d098d7a0517dd9139468c2b
thekuwayama <thekuwayama@gmail.com>
2022-02-19 22:39:03 +0900

    v0.3.3

--- pkg/brew/asaru.rb
+++ pkg/brew/asaru.rb
@@ -7,16 +7,16 @@
 #   brew untap thekuwayama/asaru
 
 class Asaru < Formula
-  version 'v0.3.2'
+  version 'v0.3.3'
   desc 'Asana Tasks Search CLI'
   homepage 'https://github.com/thekuwayama/asaru'
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/thekuwayama/asaru/releases/download/#{version}/asaru-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 '2752996c222d992d97a90884a48b7e205035d982e90f5e6eaacd15b96159f428'
+    sha256 '5ff07ae0c796c732e1b7ae042dc4c2d07e6f9ed42c994111f8cb43c495e6592a'
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/thekuwayama/asaru/releases/download/#{version}/asaru-#{version}-aarch64-apple-darwin.tar.gz"
-    sha256 '3c928139fd905c0522063717cd7f8674a1433f8d5f3d829b864d1137ae66729b'
+    sha256 '62c769f57cbc3a1f6fa85d2b602b78868d0fd31a0f336fc72303179f48dad0f7'
   else
     warn '** not support **'
     exit 1
